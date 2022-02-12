809213797c59a5d3874b79f9201f308f80df7168
gumball <ugis.rozkalns@gmail.com>
2022-02-12 11:03:59 +0800

    Initial setup - fastapi-kafka-example

---
+++ producer.py
@@ -0,0 +1,29 @@
+import json
+
+from kafka import KafkaProducer
+from fastapi import FastAPI
+
+# produce json messages
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'], value_serializer=lambda m: json.dumps(m).encode('ascii'))
+
+app = FastAPI()
+
+
+def on_send_success(record_metadata):
+    print(record_metadata.topic)
+    print(record_metadata.partition)
+    print(record_metadata.offset)
+
+
+def on_send_error(err):
+    print(err)
+    # handle exception
+
+
+@app.get("/")
+async def read_root():
+    producer.send('json-topic', {"message": "Message added"})\
+        .add_callback(on_send_success)\
+        .add_errback(on_send_error)
+    producer.flush()
+    return {"message": "Message added"}
